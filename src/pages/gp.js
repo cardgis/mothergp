d94033dca0ac8867e47538f938e7736ce3800b2b
cardgis <161532830+cardgis@users.noreply.github.com>
2024-07-05 17:56:53 +0200

    update header gp

--- src/pages/gp.js
+++ src/pages/gp.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
 
 function gp() {
   return (
-    <div>
-      <h1>Bienvenue GP</h1>
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
   );
 }
 
