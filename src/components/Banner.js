28f09c3a041bd484f3fbecf09ebd207b9e6f9254
cardgis <161532830+cardgis@users.noreply.github.com>
2024-07-05 18:55:13 +0200

    image scroll added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Image from "next/image";
+//import Image from "next/image";
 
 function Banner() {
   return (
@@ -12,20 +12,20 @@ function Banner() {
         showStatus={false}
         showIndicators={false}
         showThumbs={false}
-        interval={5000}
+        interval={3000}
       >
         <div>
           <img
             loading="lazy"
-            src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-delivery-retail-truck-association-logo-symbol-png-image_3570987.jpg"
-            alt="bannera"
+            // src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-delivery-retail-truck-association-logo-symbol-png-image_3570987.jpg"
+            // alt="bannera"
           />
         </div>
         <div>
-          <img loading="lazy" src="" alt="" />
+          <img loading="lazy" src="/scroll1.png" alt="bannera1" />
         </div>
         <div>
-          <img loading="lazy" src="" alt="" />
+          <img loading="lazy" src="/scroll2.png" alt="bannera2" />
         </div>
       </Carousel>
     </div>
